7d41cc03d07facc984310f443338c87d847bdb4b
Tuğçe Usta <tugce.gulsan@gmail.com>
2023-03-31 11:44:19 +0300

    ArrayTwoD solved

--- src/com/tugcegulsanusta/datastructure/hackerrank/array/ArrayDs.java
+++ src/com/tugcegulsanusta/datastructure/hackerrank/array/ArrayDs.java
@@ -2,26 +2,29 @@ package com.tugcegulsanusta.datastructure.hackerrank.array;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
-class Result {
+class Result2 {
 
     /*
      * Complete the 'reverseArray' function below.
      *
      * The function is expected to return an INTEGER_ARRAY.
      * The function accepts INTEGER_ARRAY a as parameter.
+     *
+     * https://www.hackerrank.com/challenges/arrays-ds/problem?isFullScreen=true
      */
 
     public static List<Integer> reverseArray(List<Integer> a) {
         // Write your code here
 
 
-        List<Integer> reversed = new ArrayList<>();
+        List<Integer> reversed = new LinkedList<>();
 
         for (int i = a.size() - 1; i >= 0; i--) {
             reversed.add(a.get(i));
@@ -42,7 +45,7 @@ public class ArrayDs {
                 .map(Integer::parseInt)
                 .collect(toList());
 
-        List<Integer> res = Result.reverseArray(arr);
+        List<Integer> res = Result2.reverseArray(arr);
 
         bufferedWriter.write(
                 res.stream()
---
+++ src/com/tugcegulsanusta/datastructure/hackerrank/array/ArrayTwoD.java
@@ -0,0 +1,65 @@
+package com.tugcegulsanusta.datastructure.hackerrank.array;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.toList;
+
+class Result1 {
+
+    /*
+     * Complete the 'hourglassSum' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int hourglassSum(List<List<Integer>> arr) {
+        // Write your code here
+        List<Integer> aSum = new ArrayList<>();
+        for (int i = 1; i < arr.size() -1; i++) {
+            for (int j = 1; j < arr.size() -1; j++) {
+                aSum.add(  //[x-1][x][x+1]
+                        arr.get(i-1).get(j-1) +
+                        arr.get(i-1).get(j)   +
+                        arr.get(i-1).get(j+1) +
+                        arr.get(i).get(j)     +
+                        arr.get(i+1).get(j-1) +
+                        arr.get(i+1).get(j)   +
+                        arr.get(i+1).get(j+1)
+                );
+            }
+        }
+        return Collections.max(aSum);
+    }
+
+}
+
+public class ArrayTwoD {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        IntStream.range(0, 6).forEach(i -> {
+            try {
+                arr.add(
+                        Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                .map(Integer::parseInt)
+                                .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        int result = Result1.hourglassSum(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
